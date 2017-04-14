d442dc5db8afac21c2c688ee3b909746671ade9c
Ettore Delprino <edelprino@gmail.com>
2017-04-14 12:49:09 +0200

    Added Job api

---
+++ tests/JobTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace ContactHub\Tests;
+
+use ContactHub\ContactHub;
+
+class JobTest extends \PHPUnit_Framework_TestCase
+{
+    const MARIO_ROSSI_CUSTOMER_ID = 'be02ac64-4d66-4756-93fc-a9e4955db639';
+
+    /** @var ContactHub */
+    private $contactHub;
+
+    public function setUp()
+    {
+        $this->contactHub = new ContactHub(Auth::TOKEN, Auth::WORKSPACE_ID, Auth::NODE_ID);
+    }
+
+    public function testAddJob()
+    {
+        $job = [
+            'id' => 'NewJobId',
+            'companyIndustry' => 'Company Industry',
+            'companyName' => 'Company Name',
+            'jobTitle' => 'Job Title',
+            'startDate' => '2017-04-03',
+            'endDate' => '2017-04-13',
+            'isCurrent' => true
+        ];
+
+        $job = $this->contactHub->addJob(static::MARIO_ROSSI_CUSTOMER_ID, $job);
+
+        assertEquals('Company Name', $job['companyName']);
+        return $job;
+    }
+
+    /**
+     * @depends testAddJob
+     */
+    public function testUpdateJob($job)
+    {
+        $job['jobTitle'] = 'New Job Title';
+
+        $job = $this->contactHub->updateJob(static::MARIO_ROSSI_CUSTOMER_ID, $job);
+
+        assertEquals('New Job Title', $job['jobTitle']);
+        return $job;
+    }
+
+    /**
+     * @depends testUpdateJob
+     */
+    public function testDeleteJob($job)
+    {
+        $this->contactHub->deleteJob(static::MARIO_ROSSI_CUSTOMER_ID, $job['id']);
+    }
+}
