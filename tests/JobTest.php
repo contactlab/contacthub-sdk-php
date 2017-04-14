a0fdee979340691c1fa804050557b0906906c97d
Ettore Delprino <edelprino@gmail.com>
2017-04-14 12:52:57 +0200

    Used trait for setup contacthub job test

--- tests/JobTest.php
+++ tests/JobTest.php
@@ -1,19 +1,11 @@
 <?php
 namespace ContactHub\Tests;
 
-use ContactHub\ContactHub;
-
 class JobTest extends \PHPUnit_Framework_TestCase
 {
     const MARIO_ROSSI_CUSTOMER_ID = 'be02ac64-4d66-4756-93fc-a9e4955db639';
 
-    /** @var ContactHub */
-    private $contactHub;
-
-    public function setUp()
-    {
-        $this->contactHub = new ContactHub(Auth::TOKEN, Auth::WORKSPACE_ID, Auth::NODE_ID);
-    }
+    use ContactHubSetUpTrait;
 
     public function testAddJob()
     {
