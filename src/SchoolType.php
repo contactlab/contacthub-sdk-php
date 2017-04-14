ee20975e1b37ebb0cdee07ac7038dbbf7a301cfb
Ettore Delprino <edelprino@gmail.com>
2017-04-14 15:13:19 +0200

    Added Education API

---
+++ src/SchoolType.php
@@ -0,0 +1,12 @@
+<?php
+namespace ContactHub;
+
+
+interface SchoolType
+{
+    const PRIMARY_SCHOOL = 'PRIMARY_SCHOOL';
+    const SECONDARY_SCHOOL = 'SECONDARY_SCHOOL';
+    const HIGH_SCHOOL = 'HIGH_SCHOOL';
+    const COLLEGE = 'COLLEGE';
+    const OTHER = 'OTHER';
+}
