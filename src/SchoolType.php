9a14c5ecb83ea05a10350d10a209a877d97e1baa
Ettore Delprino <edelprino@gmail.com>
2017-04-19 15:40:24 +0200

    Converted interface to final class

--- src/SchoolType.php
+++ src/SchoolType.php
@@ -1,9 +1,10 @@
 <?php
 namespace ContactHub;
 
-
-interface SchoolType
+final class SchoolType
 {
+    use IsValidConstant;
+
     const PRIMARY_SCHOOL = 'PRIMARY_SCHOOL';
     const SECONDARY_SCHOOL = 'SECONDARY_SCHOOL';
     const HIGH_SCHOOL = 'HIGH_SCHOOL';
